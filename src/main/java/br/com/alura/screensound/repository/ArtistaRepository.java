a015e4cbbe5ce73393f3e0baf0b5984565604d91
Abner Vital <avgchaves@gmail.com>
2025-06-14 16:02:08 -0300

    feat: adiciona repositório de Artista com busca por nome ignorando case

---
+++ src/main/java/br/com/alura/screensound/repository/ArtistaRepository.java
@@ -0,0 +1,12 @@
+package br.com.alura.screensound.repository;
+
+import br.com.alura.screensound.model.Artista;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface ArtistaRepository extends JpaRepository<Artista, Long> {
+    Optional<Artista> findByNomeContainingIgnoreCase(String nome);
+}
