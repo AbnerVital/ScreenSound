67f23b3d0dd3381b7b028aa1f30866e858b9c9dd
Abner Vital <avgchaves@gmail.com>
2025-06-15 10:01:51 -0300

    feat: exibe músicas de um artista por nome com fallback e melhorias no fluxo

--- src/main/java/br/com/alura/screensound/principal/Principal.java
+++ src/main/java/br/com/alura/screensound/principal/Principal.java
@@ -1,8 +1,10 @@
 package br.com.alura.screensound.principal;
 
 import br.com.alura.screensound.model.Artista;
+import br.com.alura.screensound.model.Carreira;
 import br.com.alura.screensound.model.Musica;
 import br.com.alura.screensound.repository.ArtistaRepository;
+import ch.qos.logback.core.encoder.JsonEscapeUtil;
 
 import java.util.List;
 import java.util.Optional;
@@ -60,6 +62,8 @@ public class Principal {
                 default:
                     System.out.println("Opção inválida!");
             }
+            System.out.println("\nPressione Enter para continuar...");
+            leitura.nextLine(); // Consome o Enter para continuar
         }
     }
 
@@ -89,6 +93,7 @@ public class Principal {
             musica.setArtista(artista.get());
             artista.get().getMusicas().add(musica);
             artistaRepository.save(artista.get());
+            System.out.println("Música cadastrada com sucesso!");
         } else {
             System.out.println("Artista não encontrado");
         }
@@ -96,14 +101,26 @@ public class Principal {
 
     private void listarMusicas() {
         List<Artista> artistas = artistaRepository.findAll();
-        artistas.forEach(System.out::println);
+        artistas.forEach(a -> a.getMusicas().forEach(System.out::println));
     }
 
     private void buscarMusicasPorArtista() {
+        System.out.println("Músicas de qual artista você quer buscar?");
+        var nome = leitura.nextLine();
+        Optional<Artista> artista = artistaRepository.findByNomeContainingIgnoreCase(nome);
+        if(artista.isPresent()){
+            System.out.println("Músicas de " + artista.get().getNome() + ": ");
+            artista.get().getMusicas().forEach(System.out::println);
+        }else {
+            System.out.println("Artista não encontrado");
+        }
 
+//        OUTRA FORMA DE BUSCAR AS MÚSICAS
+//        List<Musica> musicas = artistaRepository.buscarMusicasPorArtista(nome);
+//        musicas.forEach(System.out::println);
     }
 
     private void pesquisarDadosDoArtista() {
-
+        //Falta implementar com alguma IA que não seja o CHATGPT.
     }
 }
